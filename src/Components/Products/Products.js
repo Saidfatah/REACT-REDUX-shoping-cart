2270926de2260f807b289f3c5a72c90485e029bb
SaidFatah <said_designer@outlook.com>
2020-10-19 19:05:02 +0100

    products list and product item

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import Product from './ProductItem'
+import React,{useState,useEffect} from 'react'
+import ProductItem from './ProductItem'
+import productsData from '../../data.json'
+import Cart from './Cart'
 
 const Products=()=>{
+    const [products, setproducts] = useState([])
+    const [productsToDisplay, setproductsToDisplay] = useState([])
+    const [size, setsize] = useState("")
+    const [sort, setsort] = useState("")
+
+    useEffect(() => {
+        setproducts(productsData.products)
+        setproductsToDisplay(productsData.products)
+    }, [])
+
     return (
-        <div>
-           products 
-        </div>
+             <div className="products">
+                {products.map((product,index)=><ProductItem  key={index} product={product} />)}
+             </div>
     )
 }
 
