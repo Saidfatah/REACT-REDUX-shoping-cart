bcf69bb3894479047c71b9cc5b926f061b950a2a
SaidFatah <said_designer@outlook.com>
2020-10-20 18:53:35 +0100

    done with the modal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './Components/layout/Footer'
 import Header from './Components/layout/Header'
 import Shop from './Components/Routes/Shop'
 import About from './Components/Routes/About'
-
+import Modal from 'react-modal';
 import { BrowserRouter as Router, Switch,Route} from "react-router-dom";
 const App=()=> {
   return (
@@ -20,5 +20,7 @@ const App=()=> {
     </div>
   );
 }
+
+Modal.setAppElement('#root');
 
 export default App;
