6738c6e25d6068222b6302953d05fad2018b94d7
SaidFatah <said_designer@outlook.com>
2020-10-20 19:22:21 +0100

    more style changes

--- src/Components/Products/Cart.js
+++ src/Components/Products/Cart.js
@@ -6,22 +6,26 @@ const Cart=({cartItems,deleteCartItem,proccedeToCheckout,createOrder})=> {
      const [isCheckoutFormVisible, setisCheckoutFormVisible] = useState(false)
      return (
          <div className="cart">
-              <p>{cartItems.length>0 ? ('you have '+cartItems.length +' items in cart' ) : 'there are no items in cart '} </p>
-              {cartItems && cartItems.map((item,index)=><CartItem key={index} {...{item,deleteCartItem}} />)} 
-             <div className="cart__checkout">
-                 {
-                     cartItems.length>0 && <p>Total <span>${cartItems.map(item=>parseFloat(item.price)*item.quanitity).reduce((a, b)=> a + b , 0).toFixed(2)}</span></p>
-                 }
-                 {
-                     cartItems.length > 0
-                     ? <button   className="btn" onClick={e=>{
-                         proccedeToCheckout(e);
-                         setisCheckoutFormVisible(true)
-                     }}>Proccede</button>
-                     :null
-                 }
+             <div className="products__header">
+              <p className="products__count">you have <span>{cartItems.length}</span> items in cart</p>
              </div>
-             <Checkout {...{isCheckoutFormVisible,cartItems, createOrder,setisCheckoutFormVisible}} />
+             <div>
+                  {cartItems && cartItems.map((item,index)=><CartItem key={index} {...{item,deleteCartItem}} />)} 
+                 <div className="cart__checkout">
+                     {
+                         cartItems.length>0 && <p>Total <span>${cartItems.map(item=>parseFloat(item.price)*item.quanitity).reduce((a, b)=> a + b , 0).toFixed(2)}</span></p>
+                     }
+                     {
+                         cartItems.length > 0
+                         ? <button   className="btn" onClick={e=>{
+                             proccedeToCheckout(e);
+                             setisCheckoutFormVisible(true)
+                         }}>Proccede</button>
+                         :null
+                     }
+                 </div>
+                 <Checkout {...{isCheckoutFormVisible,cartItems, createOrder,setisCheckoutFormVisible}} />
+             </div>
          </div>
      )
 }
