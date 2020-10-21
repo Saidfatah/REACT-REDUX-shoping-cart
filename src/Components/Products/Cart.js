a9aa2e578273565828edc371979abd5ad2f34a50
SaidFatah <said_designer@outlook.com>
2020-10-21 19:39:05 +0100

    done adding redux to cart

--- src/Components/Products/Cart.js
+++ src/Components/Products/Cart.js
@@ -1,33 +1,34 @@
 import React,{useState} from 'react'
+import { connect } from 'react-redux'
 import CartItem from './CartItem'
 import Checkout from './Checkout'
+import {removeCartItem} from '../../Redux/actions/CartActions'
 
-const Cart=({cartItems,deleteCartItem,proccedeToCheckout,createOrder})=> {
+const Cart=({cartItems,removeCartItem,createOrder})=> {
      const [isCheckoutFormVisible, setisCheckoutFormVisible] = useState(false)
+
      return (
          <div className="cart">
              <div className="products__header">
               <p className="products__count">you have <span>{cartItems.length}</span> items in cart</p>
              </div>
+             
              <div>
-                  {cartItems && cartItems.map((item,index)=><CartItem key={index} {...{item,deleteCartItem}} />)} 
+                 {cartItems && cartItems.map((item,index)=><CartItem key={index} {...{item,removeCartItem,cartItems}} />)} 
                  <div className="cart__checkout">
                      {
                          cartItems.length>0 && <p>Total <span>${cartItems.map(item=>parseFloat(item.price)*item.quanitity).reduce((a, b)=> a + b , 0).toFixed(2)}</span></p>
                      }
                      {
                          cartItems.length > 0
-                         ? <button   className="btn" onClick={e=>{
-                             proccedeToCheckout(e);
-                             setisCheckoutFormVisible(true)
-                         }}>Proccede</button>
+                         ? <button  className="btn" onClick={e=>setisCheckoutFormVisible(true)}>Proccede</button>
                          :null
                      }
                  </div>
-                 <Checkout {...{isCheckoutFormVisible,cartItems, createOrder,setisCheckoutFormVisible}} />
+                 <Checkout {...{isCheckoutFormVisible,setisCheckoutFormVisible,cartItems, createOrder}} />
              </div>
          </div>
      )
 }
 
-export default Cart
+export default connect((state)=>({cartItems:state.cart.items}),{removeCartItem})(Cart)
--- src/Redux/reducers/CartReducer.js
+++ src/Redux/reducers/CartReducer.js
@@ -9,8 +9,8 @@ export const cartReducer=(state=initialState,action)=>{
     if(state == undefined) return state 
     switch (action.type) {
         case FETCH_CART_ITEMS    : return {...state, items : action.payload }
-        case ADD_PRODUCT_TO_CART : return {...state, items : action.payload.products}
-        case DELETE_CART_ITEM    : return {...state, items : action.payload.products}
+        case ADD_PRODUCT_TO_CART : return {...state, items : action.payload }
+        case DELETE_CART_ITEM    : return {...state, items : action.payload }
         default: return state
     }
 }
